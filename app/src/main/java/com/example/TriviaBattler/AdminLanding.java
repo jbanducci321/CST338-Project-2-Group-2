e38ad428d70bf11ab9854b9fb55b5d4a9e9d3919
Samantha Batson <rosemariesamantharose@gmail.com>
2025-11-11 22:17:16 -0800

    Working with changes.

--- app/src/main/java/com/example/TriviaBattler/AdminLanding.java
+++ app/src/main/java/com/example/TriviaBattler/AdminLanding.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.labandroiddemo.R;
+import com.example.TriviaBattler.R;
 
 public class AdminLanding extends AppCompatActivity {
 
