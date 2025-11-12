1e8c8f9b7101033c3c4b5519bf3487da07bf5b9b
Samantha Batson <rosemariesamantharose@gmail.com>
2025-11-11 21:26:52 -0800

    Start to Move User Landing

--- app/src/main/java/com/example/labandroiddemo/MainActivity.java
+++ app/src/main/java/com/example/labandroiddemo/MainActivity.java
@@ -6,10 +6,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.LiveData;
 
+import com.example.TriviaBattler.R;
 import com.example.labandroiddemo.database.AppRepository;
 import com.example.labandroiddemo.database.entities.User;
 import com.example.labandroiddemo.databinding.ActivityMainBinding;
@@ -154,4 +159,18 @@ public class MainActivity extends AppCompatActivity {
         return intent;
     }
 
+    public static class UserLandingPage extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_user_landing_page);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
 }
