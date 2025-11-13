d85e4c51d8daf324ec66df931fc4d2d79d0a25f7
Samantha Batson <rosemariesamantharose@gmail.com>
2025-11-12 19:58:56 -0800

    Admin Landing Implementation

--- app/src/main/java/com/example/TriviaBattler/AdminLanding.java
+++ app/src/main/java/com/example/TriviaBattler/AdminLanding.java
@@ -1,20 +1,91 @@
 package com.example.TriviaBattler;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.TriviaBattler.R;
+import androidx.lifecycle.LiveData;
+import com.example.TriviaBattler.database.AppRepository;
+import com.example.TriviaBattler.database.entities.User;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class AdminLanding extends AppCompatActivity {
+
+    private AppRepository repository;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_landing);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        repository = AppRepository.getRepository(getApplication());
+
+        int userId = getIntent().getIntExtra("USER_ID", -1);
+        if (userId != -1) {
+            LiveData<User> userObserver = repository.getUserByUserId(userId);
+            userObserver.observe(this, u -> {
+                user = u;
+                if (user != null) invalidateOptionsMenu();
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem userItem = menu.findItem(R.id.logoutMenuItem);
+        MenuItem adminItem = menu.findItem(R.id.admin);
+
+        if (user == null) {
+            userItem.setVisible(false);
+            adminItem.setVisible(false);
+            return true;
+        }
+
+        userItem.setVisible(true);
+        View actionView = userItem.getActionView();
+        if (actionView != null) {
+            TextView usernameView = actionView.findViewById(R.id.usernameTitle);
+            if (usernameView != null) usernameView.setText(user.getUsername());
+        }
+
+        adminItem.setVisible(user.isAdmin());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        } else if (id == R.id.logout) {
+            startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+            finish();
+            return true;
+        } else if (id == R.id.stats) {
+            // TODO: open stats activity
+            return true;
+        } else if (id == R.id.admin) {
+            // Already here, maybe refresh or navigate deeper
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
