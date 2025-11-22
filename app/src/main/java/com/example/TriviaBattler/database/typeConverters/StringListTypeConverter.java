7e9ca6b88d9abfe62de9de1897cee17bd57fe491
Jacob Banducci <jbanducci321@gmail.com>
2025-11-22 14:02:01 -0800

    Set up type converter for the incorrect answers Updated the Question entity Starting on Question DAO

---
+++ app/src/main/java/com/example/TriviaBattler/database/typeConverters/StringListTypeConverter.java
@@ -0,0 +1,41 @@
+package com.example.TriviaBattler.database.typeConverters;
+
+import androidx.room.TypeConverter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringListTypeConverter {
+
+    @TypeConverter
+    public String fromList(List<String> list) {
+        if (list == null) {
+            return "[]";
+        }
+        JSONArray arr = new JSONArray();
+        for (String str : list) { //Adds in the contents of the list to the json array
+            arr.put(str);
+        }
+        return arr.toString();
+    }
+
+    @TypeConverter
+    public List<String> toList(String json) {
+        List<String> list = new ArrayList<>();
+        if (json == null || json.isEmpty()) {
+            return list;
+        }
+        try {
+            JSONArray arr = new JSONArray(json);
+            for (int i = 0; i < arr.length(); ++i) {
+                list.add(arr.toString(i));
+            }
+        }
+        catch (JSONException ignored) {}
+        return list;
+    }
+
+}
