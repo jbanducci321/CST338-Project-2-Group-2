16493bca3212e4d79ad58ea08986887060df6f01
Samantha Batson <rosemariesamantharose@gmail.com>
2025-12-04 19:00:28 -0800

    comment subtract

--- app/src/test/java/com/example/TriviaBattler/IntentFactoryTest.java
+++ app/src/test/java/com/example/TriviaBattler/IntentFactoryTest.java
@@ -26,11 +26,9 @@ public class IntentFactoryTest {
 
     @Test
     public void testMainActivityIntentFactory() {
-        // Testing Main Activity's Intent Factory
         int userId = 123;
         Intent i = MainActivity.mainActivityIntentFactory(context, userId);
 
-        // Shouldn't be null, Class should match, extras should be correct
         assertNotNull(i);
         assertEquals(MainActivity.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("com.example.labandroiddemo.MAIN_ACTIVITY_USER_ID", -1));
@@ -38,32 +36,26 @@ public class IntentFactoryTest {
 
     @Test
     public void testLoginActivityIntentFactory() {
-        // Testing Login Activity's Intent Factory
         Intent i = LoginActivity.loginIntentFactory(context);
 
-        // Shouldn't be null, Class should match
         assertNotNull(i);
         assertEquals(LoginActivity.class.getName(), i.getComponent().getClassName());
     }
 
     @Test
     public void testCreateAccountActivityIntentFactory() {
-        // Testing Create Account Activity's Intent Factory
         Intent i = CreateAccountActivity.createAccountIntentFactory(context);
 
-        // Shouldn't be null, Class should match
         assertNotNull(i);
         assertEquals(CreateAccountActivity.class.getName(), i.getComponent().getClassName());
     }
 
     @Test
     public void testQuestionsIntentFactory() {
-        //  Testing Question Activity's Intent Factory
         int userId = 88;
         String diff = "easy";
         Intent i = QuestionsActivity.questionsIntentFactory(context, userId, diff);
 
-        // Shouldn't be null, Class should match, extras should be correct
         assertNotNull(i);
         assertEquals(QuestionsActivity.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("USER_ID", -1));
@@ -72,11 +64,9 @@ public class IntentFactoryTest {
 
     @Test
     public void testStatisticsIntentFactory() {
-        // Testing Stats Activity's Intent Factory
         int userId = 42;
         Intent i = Statistics.statsIntentFactory(context, userId);
 
-        // Shouldn't be null, Class should match, extras should be correct
         assertNotNull(i);
         assertEquals(Statistics.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("com.example.labandroiddemo.STATS_ACTIVITY_USER_ID", -1));
@@ -84,12 +74,10 @@ public class IntentFactoryTest {
 
     @Test
     public void testAddAdminIntentFactory() {
-        // Testing Add Admin Activity's Intent Factory
         int userId = 44;
         boolean isAdmin = true;
         Intent i = AddAdmin.addAdminIntentFactory(context, userId, isAdmin);
 
-        // Shouldn't be null, Class should match, Extras should be correct
         assertNotNull(i);
         assertEquals(AddAdmin.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("com.example.labandroiddemo.ADD_ADMIN_ACTIVITY_USER_ID", -1));
@@ -98,22 +86,18 @@ public class IntentFactoryTest {
 
     @Test
     public void testAddQuestionsAdminIntentFactory() {
-        // Testing Add Questions Activity's Intent Factory
         int userId = 14;
         Intent i = AddQuestionsAdminActivity.addQuestionIntentFactory(context, userId);
 
-        // Shouldn't be null, Class should match
         assertNotNull(i);
         assertEquals(AddQuestionsAdminActivity.class.getName(), i.getComponent().getClassName());
     }
 
     @Test
     public void testAdminLandingIntentFactory() {
-        // Testing Admin Landing Activity's Intent Factory
         int userId = 52;
         Intent i = AdminLanding.adminLandingIntentFactory(context, userId);
 
-        // Shouldn't be null, Class should match, Extras should be correct
         assertNotNull(i);
         assertEquals(AdminLanding.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("com.example.labandroiddemo.ADMIN_ACTIVITY_USER_ID", -1));
@@ -121,11 +105,9 @@ public class IntentFactoryTest {
 
     @Test
     public void testAdminStatsIntentFactory() {
-        // Testing Admin Stats Activity's Intent Factory
         int userId = 53;
         Intent i = AdminStats.adminStatsIntentFactory(context, userId);
 
-        // Shouldn't be null, Class should match, Extras should be correct
         assertNotNull(i);
         assertEquals(AdminStats.class.getName(), i.getComponent().getClassName());
         assertEquals(userId, i.getIntExtra("com.example.labandroiddemo.ADMIN_STATS_ACTIVITY_USER_ID", -1));
