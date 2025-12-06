acb25196585ccda1655facf13e3d1ec3beb2c492
Jacob Banducci <jbanducci321@gmail.com>
2025-12-06 10:13:37 -0800

    Spent a little too much time on a funny bit Worth it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,9 @@ dependencies {
     testImplementation("androidx.test.ext:junit:1.1.5")
     testImplementation("org.robolectric:robolectric:4.10.3")
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
